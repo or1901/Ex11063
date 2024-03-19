5e37f0514d371a5e4c4a35dbd7f687d81cde3640
Ori Roitzaid <oriroitzaid@gmail.com>
2024-03-19 23:12:18 +0200

    Added OnItemClickListener to the spinner, which makes that the text view displays the current selected country details

--- app/src/main/java/com/example/ex11063/MainActivity.java
+++ app/src/main/java/com/example/ex11063/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.ex11063;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner statesSpinner;
     private String[] names, capitals, populations;
     private int[] flags;
     private ArrayList<State> states;
     private StateAdapter statesAdapter;
+    private TextView stateTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initAll() {
         statesSpinner = findViewById(R.id.statesSpinner);
+        stateTv = findViewById(R.id.stateTv);
 
         names = new String[]
                 {"Israel", "United States", "France", "Germany", "Spain", "Italy", "Greece"};
@@ -46,5 +51,23 @@ public class MainActivity extends AppCompatActivity {
 
         statesAdapter = new StateAdapter(this, states);
         statesSpinner.setAdapter(statesAdapter);
+        statesSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        State state = states.get(i);
+        String text = "";
+
+        text += "Country: " + state.getName() + "\n";
+        text += "Capital: " + state.getCapital() + "\n";
+        text += "Population: " + state.getPopulation();
+
+        stateTv.setText(text);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
