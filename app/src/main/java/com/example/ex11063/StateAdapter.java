9c2ab845edfa4204edcbab5888300675d2ea8d9a
Ori Roitzaid <oriroitzaid@gmail.com>
2024-03-19 22:11:26 +0200

    Changed StateAdapter to inherit from BaseAdapter

--- app/src/main/java/com/example/ex11063/StateAdapter.java
+++ app/src/main/java/com/example/ex11063/StateAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,16 +13,32 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class StateAdapter extends ArrayAdapter<State> {
-    private ArrayList<State> stateList;
+public class StateAdapter extends BaseAdapter {
+    private ArrayList<State> statesList;
     LayoutInflater inflater;
 
-    public StateAdapter(@NonNull Context context, ArrayList<State> stateList) {
-        super(context, R.layout.state_layout, stateList);
-        this.stateList = stateList;
+    public StateAdapter(@NonNull Context context, ArrayList<State> statesList) {
+        this.statesList = statesList;
         inflater = (LayoutInflater.from(context));
     }
 
+    @Override
+    public int getCount() {
+        return statesList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return statesList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @NonNull
+    @Override
     public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
         view = inflater.inflate(R.layout.state_layout, parent, false);
 
@@ -30,7 +46,7 @@ public class StateAdapter extends ArrayAdapter<State> {
         TextView stateNameTv = view.findViewById(R.id.stateNameTv);
         TextView stateCapitalTv = view.findViewById(R.id.stateCapitalTv);
 
-        State state = this.stateList.get(position);
+        State state = statesList.get(position);
 
         stateIv.setImageResource(state.getImageId());
         stateNameTv.setText(state.getName());
